2fa72a7fd5cff578675eac6dbce2a14b8c7202cb
David Trinchi <trinchi.davide@gmail.com>
2019-03-05 01:03:46 +0100

    Add loading screen and icons

--- src/index.js
+++ src/index.js
@@ -1,24 +1,23 @@
 const {app, BrowserView, BrowserWindow} = require('electron');
-
-const json = require('../res/settings');
-
+const settings = require('../res/settings');
 const path = require('path');
-
 const {autoUpdater} = require("electron-updater");
 
 autoUpdater.logger = require("electron-log");
 autoUpdater.logger.transports.file.level = "info";
 
-
 let mainWindow;
+let loadingScreen;
 
 function createWindow() {
+
     mainWindow = new BrowserWindow({
         minWidth: 900,
         minHeight: 600,
         width: 900,
         height: 600,
-        titleBarStyle: "hidden"
+        titleBarStyle: "hidden",
+        show: false
     });
 
     let browserView = new BrowserView({
@@ -43,18 +42,36 @@ function createWindow() {
 
     browserView.setBounds({
         x: 0,
-        y: 15,
+        y: 20,
         width: 900,
-        height: 585
+        height: 580
     });
 
-    browserView.webContents.loadURL(json.url);
+    browserView.webContents.loadURL(settings.url);
 
     mainWindow.on('closed', () => {
         mainWindow = null
     });
+
+    setTimeout( () => {
+        loadingScreen.hide();
+        mainWindow.show();
+    }, 2000);
+
 }
 
+function createLoadingScreen() {
+
+    loadingScreen = new BrowserWindow({
+        width: 300,
+        height: 300,
+        frame: false
+    });
+
+    loadingScreen.loadURL(path.join('file://', __dirname, '/loadingScreen.html'));
+}
+
+app.on('ready', createLoadingScreen);
 app.on('ready', createWindow);
 
 app.on('ready', function() {
@@ -68,7 +85,7 @@ app.on('window-all-closed', function() {
 app.on('activate', function() {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (win === null) {
-        createWindow()
+    if (mainWindow === null) {
+        createWindow();
     }
 });
